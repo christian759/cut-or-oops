add46366f0006ce31e4ef7944f2ba250ebc2bbb2
ceo1 <christian4onos@outlook.com>
2026-03-01 20:18:11 +0100

    Remove splash screen, update `Splash.tscn` background color, and apply minor UI adjustments to `home.tscn` including adding unique IDs and reordering button style properties.

--- Scripts/splash.gd
+++ Scripts/splash.gd
@@ -1,10 +1,10 @@
 extends Control
 
-func _ready() -> void:
+# func _ready() -> void:
 	# Set simple, colorful full rect sizing
-	anchors_preset = Control.PRESET_FULL_RECT
-	size = get_viewport_rect().size
+	# anchors_preset = Control.PRESET_FULL_RECT
+	# size = get_viewport_rect().size
 	
 	# Transition to Home scene after a 1.5-second delay
-	await get_tree().create_timer(1.5).timeout
-	get_tree().change_scene_to_file("res://Scenes/home.tscn")
+	# await get_tree().create_timer(1.5).timeout
+	# get_tree().change_scene_to_file("res://Scenes/home.tscn")
